d0bcb2cf01169498e8b70e6d8214a349e7ed7d15
Enrico-N <ngenricok@gmail.com>
2022-08-10 20:57:03 -0700

    removed unnecessary files

--- server/server.js
+++ server/server.js
@@ -8,8 +8,6 @@ app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: "true" }));
 
-app.get("/", (resposne, request) => {});
-
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server running on`, 5000);
 });
