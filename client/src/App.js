0fc21215a65f3735b50d369c5d4d7ab876ad9398
Enrico-N <ngenricok@gmail.com>
2022-08-15 14:18:46 -0700

    temp

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Todo from "./components/Todo";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+// import Todo from "Todo.js";
+
+const URL = process.env.REACT_APP_URL;
+
+console.log(URL);
+
+async function getTodo() {
+  let response = await fetch("/api/addTodo");
+  let data = await response;
+  console.log(data);
+  return data;
 }
+
+const App = () => {
+  useEffect(() => {
+    getTodo(URL);
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Todo List: </h1>
+      <Todo />
+
+      <h2>Add A Todo:</h2>
+
+      <form action="/addTodo" method="POST">
+        <input type="text" placeholder="Thing To Do" name="todoItem" />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,34 +1,47 @@
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config({ path: "./.env" });
 
+//IMPORTS
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT;
-
 const bodyParser = require("body-parser");
+const { json } = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
+
+const PORT = process.env.PORT || 5000;
+
+//MIDDLEWARE
+app.use(cors());
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: "true" }));
 app.use(express.json());
 
+//MONGODB DATABASE
 let db;
 let dbConnectionStr = process.env.DB_STRING;
 let dbName = "todo";
 
+//CONNECTION TO MONGODB DATABASE
 MongoClient.connect(dbConnectionStr, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then((client) => {
-  console.log(`Connect to ${dbName} Database`);
-  db = client.db(dbName);
 });
+async (client) => {
+  try {
+    console.log(`Connect to ${dbName} Database`);
+    db = await client.db(dbName);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 app.get("./", async (request, response) => {
   const itemData = await db.collection("todos").find().toArray();
   const itemsLeft = await db
     .collection("todos")
     .countDocuments({ completed: false });
-  response.render("../.index.html", { items: todoItems, left: itemsLeft });
+  // response.render("../../.index.html", { items: todoItems, left: itemsLeft });
   console.log(itemData);
 });
 
